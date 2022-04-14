65b2a4b75f528dbb8884ca50326e9b60af8428c8
Ahmed Noori <moe7ca@gmail.com>
2022-04-14 06:21:41 -0400

    Add login and signup

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+  
+    def create
+     
+        user = User.find_by_email(params[:email])
+            if user = User.authenticate_with_credentials(params[:email], params[:password])
+              # success logic, log them in
+              session[:user_id] = user.id
+            redirect_to '/'
+            else
+              # failure, render login form
+              redirect_to '/login'
+            end
+        
+           end
+  
+    def destroy
+        session[:user_id] = nil
+        redirect_to '/login'
+    end
+  
+  end
