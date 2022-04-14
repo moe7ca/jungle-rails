19ed2e9bbb02ac6d1e45871947498ce2e7851be9
Ahmed Noori <moe7ca@gmail.com>
2022-04-14 05:50:13 -0400

    Add login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,23 @@ Rails.application.routes.draw do
     post   :add_item
     post   :remove_item
   end
-
+  resources :about, only: [:index]
   resources :orders, only: [:create, :show]
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
-
+    
+    # these routes are for showing users a login form, logging them in, and logging them out.
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
+  
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+    
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
